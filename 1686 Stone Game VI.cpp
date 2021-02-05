bfa0baaf092c8454d5c47ed34c1d35f229f987cb
Manish Kumar Rajak <manishkumar.jnv12@gmail.com>
2021-02-05 10:27:27 +0530

    Solution for problem 1686

---
+++ 1686 Stone Game VI.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+		int n = aliceValues.size();
+		vector<pair<int, int> > valuesSum(n);
+		for (int i = 0; i < n; i++)
+			valuesSum[i] = make_pair(aliceValues[i] + bobValues[i], i);
+		sort(valuesSum.begin(), valuesSum.end(), greater<pair<int, int> >());
+		int alicePts = 0, bobPts = 0;
+		for (int i = 0; i < n; i++) {
+			int idx = valuesSum[i].second;
+			if (i % 2)
+				bobPts += bobValues[idx];
+			else
+				alicePts += aliceValues[idx];
+		}
+		if (alicePts > bobPts)
+			return 1;
+		else if (alicePts < bobPts)
+			return -1;
+		return 0;
+	}
+};
