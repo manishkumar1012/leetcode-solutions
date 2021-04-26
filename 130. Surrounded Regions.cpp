9b5a81b1e35aaeb7a77ac70fab896cf3ab9a1576
Manish Kumar <50907523+manishkumar1012@users.noreply.github.com>
2021-04-26 09:19:39 +0530

    Update 130. Surrounded Regions.cpp

--- 130. Surrounded Regions.cpp
+++ 130. Surrounded Regions.cpp
@@ -1,13 +1,12 @@
-// Problem Link: https://leetcode.com/problems/surrounded-regions/
+// // Problem Link: https://leetcode.com/problems/surrounded-regions/
 
-// Idea : Using DFS to traverse the boundaries, when 'O' is found, fire the dfs() to flip all the connected 'O' into ' * '.
-// After that, go through the board and flip the other 'O' to 'X', since these 'O' cannot escape the board. Then flip all the '*' to 'O', indicating these 'O' can reach out of the board.
+// // Idea : Using DFS to traverse the boundaries, when 'O' is found, fire the dfs() to flip all the connected 'O' into ' * '.
+// // After that, go through the board and flip the other 'O' to 'X', since these 'O' cannot escape the board. Then flip all the '*' to 'O', indicating these 'O' can reach out of the board.
 
 class Solution {
 public:
 	void dfs(int i, int j, vector<vector<char>>& board) {
-        // don't consider border row and column
-		if (i <= 0 || j <= 0 || i >= board.size() - 1 || j >= board[0].size() - 1)
+		if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size())
 			return;
 		if (board[i][j] == 'X' || board[i][j] == '*')
 			return;
@@ -27,26 +26,23 @@ public:
 
         // first and last row
         for (int i = 0; i < n; i++) {
-            // don't call dfs on 1st row
         	if (board[0][i] == 'O')
-        		dfs(1, i, board); 
+        		dfs(0, i, board); 
         	if (board[m - 1][i] == 'O')
-        		dfs(m - 2, i, board);
+        		dfs(m - 1, i, board);
         }
 
         // first and last column
         for (int i = 0; i < m; i++) {
-            // don't call dfs on 1st column
         	if (board[i][0] == 'O')
-        		dfs(i, 1, board);
+        		dfs(i, 0, board);
         	if (board[i][n - 1] == 'O')
-        		dfs(i, n - 2, board);
+        		dfs(i, n - 1, board);
         }
 
         // flip O to X, '*' to 'O',
-        // skip the boulders
-        for (int i = 1; i < m - 1; i++) {
-        	for (int j = 1; j < n - 1; j++) {
+        for (int i = 0; i < m; i++) {
+        	for (int j = 0; j < n; j++) {
         		if (board[i][j] == 'O')
         			board[i][j] = 'X';
         		else if (board[i][j] == '*')
