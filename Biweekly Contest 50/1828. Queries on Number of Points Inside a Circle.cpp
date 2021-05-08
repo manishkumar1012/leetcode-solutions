78f0aebc7042bc04745e618036a667395053705a
Manish Kumar <50907523+manishkumar1012@users.noreply.github.com>
2021-05-08 16:32:06 +0530

    Update 1828. Queries on Number of Points Inside a Circle.cpp

--- Biweekly Contest 50/1828. Queries on Number of Points Inside a Circle.cpp
+++ Biweekly Contest 50/1828. Queries on Number of Points Inside a Circle.cpp
@@ -1,3 +1,4 @@
+// Brute Force Solution
 class Solution {
 public:
     vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
@@ -11,6 +12,37 @@ public:
         	for (auto point : points) {
         		int x = point[0];
         		int y = point[1];
+
+        		if (((x - centreX) * (x - centreX)) + ((y - centreY) * (y - centreY)) <= radius * radius)
+        			cnt++;
+        	}
+        	res.push_back(cnt);
+        }
+        return res;
+    }
+};
+
+// Optimized Solution
+class Solution {
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        vector<int> res;
+        
+        sort(points.begin(), points.end());
+        for (auto query : queries) {
+        	int centreX = query[0];
+        	int centreY = query[1];
+        	int radius = query[2];
+        	int cnt = 0;
+
+        	int ind = lower_bound(points.begin(), points.end(), vector<int>{centreX - radius, 0}) - points.begin();
+
+        	for (; ind < points.size(); ind++) {
+                auto point = points[ind];
+        		int x = point[0];
+        		int y = point[1];
+        		if (x > centreX + radius)
+        			break;
 
         		if (((x - centreX) * (x - centreX)) + ((y - centreY) * (y - centreY)) <= radius * radius)
         			cnt++;
